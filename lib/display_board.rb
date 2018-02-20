b3238b4542bc7a74649bd3c73be11cd3bf886d01
Elizabeth Eckels <elizabeth.r.eckels@gmail.com>
2018-02-20 04:41:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board=[" "," "," "," "," "," "," "," "," "]
+board=[" "," "," "," ","X"," "," "," "," "]
 
 puts "Welcome to Tic Tac Toe"
 
